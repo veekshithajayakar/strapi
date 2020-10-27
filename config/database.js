b13a5586797ee46ec1ebdb70818cce7985ff5c0b
Veekshitha Shetty <veekshitha.shetty@mavenir.com>
2020-10-27 12:37:17 +0530

    added some db config changes

--- config/database.js
+++ config/database.js
@@ -8,11 +8,12 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'postgres'),
-        ssl: env.bool('DATABASE_SSL', false),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
-      options: {}
+      options: {
+        ssl: false,
+      },
     },
   },
 });
